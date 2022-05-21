620054cf83d4056d1228147e4c17ec51958cea33
KirillVN <kirill137777@gmail.com>
2022-05-21 21:25:55 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,24 @@
+const reviews = document.querySelectorAll(".review-content");
+let currentSlide = 0;
+
+window.addEventListener("click", changeReview);
+
+function changeReview(e) {
+    let target = e.target
+    if(target.classList.contains("btn") && target.classList.contains("prev")) {
+        currentSlide--;
+        if(currentSlide < 0) {currentSlide = reviews.length - 1}
+        showSlide()
+    } else if (target.classList.contains("btn") && target.classList.contains("next")){
+        currentSlide++;
+        if(currentSlide > reviews.length - 1) {currentSlide = 0}
+        showSlide()
+    } 
+}
+
+function showSlide() {
+    reviews.forEach((review) => {
+        review.classList.remove("active-review")
+    })
+    reviews[currentSlide].classList.add("active-review")
+}
