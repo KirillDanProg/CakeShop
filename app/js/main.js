094bae72b5fd1b60a2d6c4148ee4bcfea7287044
KirillVN <kirill137777@gmail.com>
2022-05-23 08:42:20 +0300

    parallax added

--- app/js/main.js
+++ app/js/main.js
@@ -3,14 +3,33 @@ const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const btns = document.querySelectorAll(".btn");
 const banner = document.querySelector(".banner-img");
-const bannerSlides = document.querySelectorAll(".banner-img img")
+const bannerSlides = document.querySelectorAll(".banner-img img");
+const bannerBtns = document.querySelectorAll(".btn-banner");
+
 let currentSlide = 0;
 let index = 0;
 let isPlaying = true;
+let intervalID = null;
 
 next.addEventListener("click", nextBanner);
-prev.addEventListener("click", prevBanner)
+prev.addEventListener("click", prevBanner);
 btns.forEach((btn) => btn.addEventListener("click", changeReview));
+banner.addEventListener("transitionend", transitioтFunc);
+
+startSlider()
+
+function transitioтFunc() {
+  if (index / 100 > bannerSlides.length - 2) {
+    banner.style.transition = "none";
+    index = 100;
+    banner.style.transform = `translate(-${index}%)`;
+  }
+  if (index === 0) {
+    banner.style.transition = "none";
+    index = 400;
+    banner.style.transform = `translate(-${index}%)`;
+  }
+}
 
 function changeReview(e) {
   let target = e.target;
@@ -48,8 +67,7 @@ function nextBanner() {
   banner.style.transform = `translate(-${index}%)`;
 }
 function prevBanner() {
-    // isPlaying = false;
-    index -= 100;
+  index -= 100;
   if (index < 0) {
     index = (bannerSlides.length - 2) * 100;
   }
@@ -57,25 +75,26 @@ function prevBanner() {
   banner.style.transform = `translate(-${index}%)`;
 }
 
-banner.addEventListener("transitionend", () => {
-    if(index / 100 > bannerSlides.length - 2) {
-        banner.style.transition = "none";
-        index = 100;
-        banner.style.transform = `translate(-${index}%)`;
-    }
-    if(index === 0) {
-        banner.style.transition = "none";
-        index = 400;
-        banner.style.transform = `translate(-${index}%)`
-    }
-})
-if(isPlaying) {
-    let intervalID = setInterval(() => {
-        nextBanner()
-    }, 1900)
+bannerBtns.forEach((bannerBtn) => {
+  bannerBtn.addEventListener("click", stopSlider);
+});
+
+function stopSlider() {
+  isPlaying = false;
+  clearInterval(intervalID);
+  console.log(isPlaying);
+  setTimeout(() => {
+    isPlaying = true;
+    startSlider()
+  }, 1900);
 }
-
-
+function startSlider() {
+  if (isPlaying) {
+    intervalID = setInterval(() => {
+      nextBanner();
+    }, 1900);
+  }
+}
 
 // TRANSFORM TRANSLATE VARIANT
 // btnNext.addEventListener("click", () => {
