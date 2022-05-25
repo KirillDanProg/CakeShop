7356b0ac38ee34e06917583df019e09b6e7a2119
KirillVN <kirill137777@gmail.com>
2022-05-25 08:54:18 +0300

    modal added

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,6 @@ const banner = document.querySelector(".banner-img");
 const bannerSlides = document.querySelectorAll(".banner-img img");
 const bannerBtns = document.querySelectorAll(".btn-banner");
 
-
 let currentSlide = 0;
 let index = 0;
 let isPlaying = true;
@@ -16,8 +15,6 @@ next.addEventListener("click", nextBanner);
 prev.addEventListener("click", prevBanner);
 btns.forEach((btn) => btn.addEventListener("click", changeReview));
 banner.addEventListener("transitionend", transitioтFunc);
-
-startSlider()
 
 function transitioтFunc() {
   if (index / 100 > bannerSlides.length - 2) {
@@ -64,7 +61,7 @@ function nextBanner() {
   if (index / 100 > bannerSlides.length - 1) {
     index = 0;
   }
-  banner.style.transition = "1s";
+  banner.style.transition = "0.8s";
   banner.style.transform = `translate(-${index}%)`;
 }
 function prevBanner() {
@@ -72,10 +69,9 @@ function prevBanner() {
   if (index < 0) {
     index = (bannerSlides.length - 2) * 100;
   }
-  banner.style.transition = "1s";
+  banner.style.transition = "0.8s";
   banner.style.transform = `translate(-${index}%)`;
 }
-
 
 bannerBtns.forEach((bannerBtn) => {
   bannerBtn.addEventListener("click", stopSlider);
@@ -94,3 +90,57 @@ function startSlider() {
   }
 }
 
+startSlider();
+
+// MODAL JS
+
+const menuItems = document.querySelectorAll(".menu__item");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", showModal);
+});
+
+function showModal(e) {
+  let target = e.target;
+  let modalImg = target.parentNode.children[0].getAttribute("src")
+  let modalName = target.parentNode.children[1].innerHTML
+  let modalPrice = target.parentNode.children[2].innerHTML
+
+  if (
+    target.classList.contains("cake__name") ||
+    target.classList.contains("cake__img")
+  ) {
+    e.preventDefault();
+
+    header.innerHTML += createModal(modalImg, modalName, modalPrice)
+    const modal = document.querySelector(".modal");
+    modal.classList.add("active-modal")
+    const closeBtn = document.querySelector(".close-btn");
+    closeBtn.addEventListener("click", ()=> {
+      header.lastElementChild.remove()
+    })
+  }
+}
+
+function createModal(img, name, price) {
+  return `
+    <section class="modal">
+    <div class="modal-container">
+      <i class="fa-solid fa-xmark close-btn"></i>
+      <img src="${img}">
+      <h3>${name}</h3>
+      <p class="price">${price}</p>
+      <h6>Состав:</h6>
+      <p class="info">Яйцо, сливки животные, крем для взбивания Шантипак, сахар, крахмал кукурузный, разрыхлитель, уксус, малина, сахарная пудра.
+        Без глютена.</p>
+      <h6>Масса:</h6>
+      <p class="info">2000кг</p>
+      <h6>Энергетическая ценность:</h6>
+      <p class="info">белки- 2,2 г; жиры- 9,3 г; углеводы- 34,2г / 100 г</p>
+      <a href="#order">Заказать</a>
+    </div>
+  </section>
+    `;
+}
